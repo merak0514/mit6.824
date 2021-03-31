28ed72e5dffbe6bbb1d97ae5a24a0111048f018c
panhaoxuan <panhaoxuan@bytedance.com>
2021-03-31 20:48:03 +0800

    自动运行的命令行

---
+++ src/main/phx.sh
@@ -0,0 +1,8 @@
+# shellcheck disable=SC2046
+kill -9 $(lsof -i :1234|sed -n '2p'|awk '{print $2}')
+go build -race -buildmode=plugin ../mrapps/wc.go
+go run -race mrcoordinator.go pg-*.txt&
+sleep 5
+go run -race mrworker.go wc.so
+sleep 5
+kill -9 $(lsof -i :1234|sed -n '2p'|awk '{print $2}')
